e3c29b2ae3153ffbea93354d97dba3f496788eca
Md-Easin-Ali-Khane <mdeasinalikhan9@gmail.com>
2026-02-22 21:35:11 +0600

    Completed the document section

---
+++ script.js
@@ -0,0 +1,6 @@
+let totalNumber = document.getElementById("total");
+let interviewNumber = document.getElementById("interview");
+let rejectedNumber = document.getElementById("rejected");
+
+let cards = document.getElementById("cards")
+console.log(cards.children.length)
